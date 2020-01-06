6758d6636aa38630b3ba24b45b7ad5ce4f9825b9
sl1673495 <949589855@qq.com>
2020-01-06 23:32:56 +0800

    feat: 优化vuex类型推断

--- vuex.ts
+++ vuex.ts
@@ -1,69 +1,103 @@
 export type Actions<S, A> = {
-  [key in keyof A]: (state: S, payload: any) => Promise<any>;
-};
+  [key in keyof A]: (state: S, payload: any) => Promise<any>
+}
 
 export type ActionArguments<A> = {
-  type: keyof A;
-  payload?: any;
-};
+  type: keyof A
+  payload?: any
+}
 
-export type Subscriber<S, A> = (action: ActionArguments<A>, state: S) => any;
+export type Subscriber<S, A> = (action: ActionArguments<A>, state: S) => any
 
 export type ActionSubscriber<S, A> = {
-  before: Subscriber<S, A>;
-  after?: Subscriber<S, A>;
-};
+  before: Subscriber<S, A>
+  after?: Subscriber<S, A>
+}
 
-export type ActionSubscribers<S, A> = ActionSubscriber<S, A>[];
+export type ActionSubscribers<S, A> = ActionSubscriber<S, A>[]
 
-export default class Vuex<S extends {}, A extends Actions<S, A>> {
-  state: S;
+export interface Dispatch<A> {
+  (action: A, ...extraArgs: any[]): any
+}
 
-  action: Actions<S, A>;
+export default class Vuex<S, A> {
+  state: S
 
-  _actionSubscribers: ActionSubscribers<S, A>;
+  action: Actions<S, A>
 
-  constructor({ state, action }: { state: S; action: Actions<S, A> }) {
-    this.state = state;
-    this.action = action;
-    this._actionSubscribers = [];
+  _actionSubscribers: ActionSubscribers<S, A>
+
+  constructor({state, action}: {state: S; action: Actions<S, A>}) {
+    this.state = state
+    this.action = action
+    this._actionSubscribers = []
   }
 
   dispatch(action: ActionArguments<A>) {
-    this._actionSubscribers.forEach(sub => sub.before(action, this.state));
+    this._actionSubscribers.forEach(sub => sub.before(action, this.state))
 
-    const { type, payload } = action;
+    const {type, payload} = action
     this.action[type](this.state, payload).then(() => {
-      this._actionSubscribers.forEach(sub => sub.after(action, this.state));
-    });
+      this._actionSubscribers.forEach(sub => sub.after(action, this.state))
+    })
   }
 
   subscribeAction(subscriber: ActionSubscriber<S, A>) {
-    this._actionSubscribers.push(subscriber);
+    this._actionSubscribers.push(subscriber)
+  }
+
+  createDispatch<A>() {
+    return this.dispatch.bind(this) as Dispatch<A>
   }
 }
 
 const store = new Vuex({
   state: {
     count: 0,
+    message: '',
   },
   action: {
     async add(state, payload) {
-      state.count += payload;
+      state.count += payload
+    },
+    async chat(state, message) {
+      state.message = message
     },
   },
-});
+})
 
 store.subscribeAction({
   before: (action, state) => {
-    console.log(`before action ${action.type}, before count is ${state.count}`);
+    console.log(
+      `before action ${action.type}, before state is ${JSON.stringify(state)}`,
+    )
   },
   after: (action, state) => {
-    console.log(`after action ${action.type},  after count is ${state.count}`);
+    console.log(
+      `after action ${action.type},  after state is ${JSON.stringify(state)}`,
+    )
   },
-});
+})
 
-store.dispatch({
+type ActionTypes =
+  | {
+      type: 'add'
+      payload: number
+    }
+  | {
+      type: 'chat'
+      payload: string
+    }
+
+// for TypeScript support
+const dispatch = store.createDispatch<ActionTypes>()
+
+dispatch({
   type: 'add',
-  payload: 2,
-});
+  payload: 3,
+})
+
+dispatch({
+  type: 'chat',
+  payload: 'Hello World',
+})
