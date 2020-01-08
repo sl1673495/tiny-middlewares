ae8ae720f9b267ba04cfe27943c3d232881ae201
ssh <949589855@qq.com>
2020-01-08 11:10:16 +0800

    feat(koa): add koa middlewares

---
+++ koa.ts
@@ -0,0 +1,58 @@
+type Context = {
+  req: string;
+  res: any;
+};
+type Middleware = (ctx: Context, next: Function) => void;
+
+function composeMiddlewares(middlewares: Middleware[]) {
+  return function wrapMiddlewares(ctx: Context) {
+    let index = -1;
+    function dispatch(i: number) {
+      index = i;
+      const fn = middlewares[i];
+      if (!fn) return Promise.resolve();
+      return Promise.resolve(fn(ctx, () => dispatch(i + 1)));
+    }
+    return dispatch(0);
+  };
+}
+
+class Koa {
+  middlewares: Middleware[] = [];
+
+  use(middleware: Middleware) {
+    this.middlewares.push(middleware);
+  }
+
+  start({ req }) {
+    const composed = composeMiddlewares(this.middlewares);
+    const ctx = { req, res: undefined };
+    return composed(ctx);
+  }
+}
+
+const app = new Koa();
+
+// log middleware
+app.use(async (ctx, next) => {
+  const { req } = ctx;
+  console.log(`req is ${JSON.stringify(req)}`);
+  await next();
+  console.log(`res is ${JSON.stringify(ctx.res)}`);
+});
+
+// service middleware
+app.use(async (ctx) => {
+  const { req } = ctx;
+
+  console.log(`calculating the res of ${req}...`);
+  const res = {
+    code: 200,
+    result: `req ${req} success`,
+  };
+
+  // 写入ctx
+  ctx.res = res;
+});
+
+app.start({ req: 'ssh' });
--- redux.ts
+++ redux.ts
@@ -1,98 +1,90 @@
-type Reducer<S, A> = (state: S, action: A) => S
+type Reducer<S, A> = (state: S, action: A) => S;
 
 type Action<T = any> = {
-  type: T
-}
+  type: T;
+};
 
 function compose(...funcs: Function[]) {
-  return funcs.reduce((a, b) => (...args: any) => a(b(...args)))
+  return funcs.reduce((a, b) => (...args: any) => a(b(...args)));
 }
 
-function createStore<S, A extends Action>(
-  reducer: Reducer<S, A>,
-  middlewares?: any[],
-) {
-  let currentState: S
+function createStore<S, A extends Action>(reducer: Reducer<S, A>, middlewares?: any[]) {
+  let currentState: S;
 
   function dispatch(action: A) {
-    currentState = reducer(currentState, action)
+    currentState = reducer(currentState, action);
   }
 
   function getState() {
-    return currentState
+    return currentState;
   }
 
-  dispatch({type: 'INIT'} as A)
+  dispatch({ type: 'INIT' } as A);
 
-  let enhancedDispatch = dispatch
+  let enhancedDispatch = dispatch;
   if (middlewares) {
-    enhancedDispatch = compose(...middlewares)(dispatch, getState)
+    enhancedDispatch = compose(...middlewares)(dispatch);
   }
 
   return {
     dispatch: enhancedDispatch,
     getState,
-  }
+  };
 }
 
 type AddAction = {
-  type: 'add'
-  payload: number
-}
+  type: 'add';
+  payload: number;
+};
 
 type ChatAction = {
-  type: 'chat'
-  message: string
-}
+  type: 'chat';
+  message: string;
+};
 
 const counterInitial = {
   count: 0,
   message: '',
-}
+};
 
-function counterReducer(
-  state = counterInitial,
-  action: AddAction | ChatAction,
-) {
+function counterReducer(state = counterInitial, action: AddAction | ChatAction) {
   switch (action.type) {
     case 'add': {
       return {
         ...state,
         count: state.count + action.payload,
-      }
+      };
     }
 
     case 'chat': {
       return {
         ...state,
         message: action.message,
-      }
+      };
     }
 
     default: {
-      return state
+      return state;
     }
   }
 }
 
-const typeLogMiddleware = (dispatch) => {
-    return ({type, ...args}) => {
-        console.log(`type is ${type}`)
-        return dispatch({type, ...args})
-    }
-}
+const typeLogMiddleware = dispatch => {
+  return ({ type, ...args }) => {
+    console.log(`type is ${type}`);
+    return dispatch({ type, ...args });
+  };
+};
 
-const stateLogMiddleware = (dispatch, getState) => {
-    return ({type, ...args}) => {
-        console.log(`state before is ${JSON.stringify(getState())}`)
-        const result = dispatch({type, ...args})
-        console.log(`state after is ${JSON.stringify(getState())}`)
-        return result
-    }
-}
+const otherDummyMiddleware = dispatch => {
+  return action => {
+    console.log(`type in dummy is ${action.type}`);
+    return dispatch(action);
+  };
+};
 
-const counterStore = createStore(counterReducer, [typeLogMiddleware, stateLogMiddleware])
+const counterStore = createStore(counterReducer, [typeLogMiddleware, otherDummyMiddleware]);
 
-console.log(counterStore.getState().count)
-counterStore.dispatch({type: 'add', payload: 2})
-console.log(counterStore.getState().count)
+console.log(counterStore.getState().count);
+counterStore.dispatch({ type: 'add', payload: 2 });
+console.log(counterStore.getState().count);
