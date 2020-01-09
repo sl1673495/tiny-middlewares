fae20d1f612cd17bfc18d95e8a2d66a6e6d27372
ssh <949589855@qq.com>
2020-01-09 12:02:41 +0800

    fix: optimize vuex types

--- vuex.ts
+++ vuex.ts
@@ -1,118 +1,111 @@
 export type Actions<S, A> = {
-  [K in keyof A]: (state: S, payload: any) => Promise<any>
-}
+  [K in keyof A]: (state: S, payload: any) => Promise<any>;
+};
 
 export type ActionArguments<A> = {
-  type: keyof A
-  payload?: any
-}
+  type: keyof A;
+  payload?: any;
+};
 
-export type Subscriber<S, A, G> = (
-  action: ActionArguments<A>,
-  state: S,
-  getters: G,
-) => any
+export type Subscriber<S, A, G> = (action: ActionArguments<A>, state: S, getters: G) => any;
 
 export type ActionSubscriber<S, A, G> = {
-  before: Subscriber<S, A, G>
-  after?: Subscriber<S, A, G>
-}
+  before: Subscriber<S, A, G>;
+  after: Subscriber<S, A, G>;
+};
 
-export type ActionSubscribers<S, A, G> = ActionSubscriber<S, A, G>[]
+export type ActionSubscribers<S, A, G> = ActionSubscriber<S, A, G>[];
 
 export interface Dispatch<A> {
-  (action: A, ...extraArgs: any[]): any
+  (action: A, ...extraArgs: any[]): any;
 }
 
 export type Getters<S, G> = {
-  [K in keyof G]: (state: S) => G[K]
-}
+  [K in keyof G]: (state: S) => G[K];
+};
 
 export type PickPayload<Types, Type> = Types extends {
-  type: Type
-  payload: infer P
+  type: Type;
+  payload: infer P;
 }
   ? P
-  : never
+  : never;
 
+type A = string | number;
+type PickString<T> = T extends string ? T : never;
+
+type T1 = PickString<A>; // string
 export default class Vuex<S, A, G> {
-  state: S
-
-  action: Actions<S, A>
-
+  state: S;
+  action: Actions<S, A>;
   // getters的计算规则
-  rawGetters: Getters<S, G>
+  rawGetters: Getters<S, G>;
   // 计算出的getters
-  getters: G
-
-  _actionSubscribers: ActionSubscribers<S, A, G>
+  getters: G;
+  _actionSubscribers: ActionSubscribers<S, A, G>;
 
   constructor({
     state,
     action,
     getters: rawGetters,
   }: {
-    state: S
-    action: Actions<S, A>
-    getters: Getters<S, G>
+    state: S;
+    action: Actions<S, A>;
+    getters: Getters<S, G>;
   }) {
-    this.state = state
-    this.action = action
-    this._actionSubscribers = []
-    this.rawGetters = rawGetters
-    this.getters = {} as G
-    this.calcGetters()
+    this.state = state;
+    this.action = action;
+    this._actionSubscribers = [];
+    this.rawGetters = rawGetters;
+    this.getters = {} as G;
+    this.calcGetters();
   }
 
   dispatch(action: ActionArguments<A>) {
-    this._actionSubscribers.forEach(sub =>
-      sub.before(action, this.state, this.getters),
-    )
+    this._actionSubscribers.forEach(sub => sub.before(action, this.state, this.getters));
 
-    const {type, payload} = action
+    const { type, payload } = action;
     this.action[type](this.state, payload).then(() => {
-      this._actionSubscribers.forEach(sub =>
-        sub.after(action, this.state, this.getters),
-      )
+      this._actionSubscribers.forEach(sub => sub.after(action, this.state, this.getters));
       // 计算一把getters
-      this.calcGetters()
-    })
+      this.calcGetters();
+    });
   }
 
   subscribeAction(subscriber: ActionSubscriber<S, A, G>) {
-    this._actionSubscribers.push(subscriber)
+    this._actionSubscribers.push(subscriber);
   }
 
   calcGetters() {
-    const {rawGetters} = this
+    const { rawGetters } = this;
     Object.keys(this.rawGetters).forEach(rawGetterKey => {
-      const rawGetter = rawGetters[rawGetterKey]
-      this.getters[rawGetterKey] = rawGetter(this.state)
-    })
+      const rawGetter = rawGetters[rawGetterKey];
+      this.getters[rawGetterKey] = rawGetter(this.state);
+    });
   }
 
   createDispatch<A>() {
-    return this.dispatch.bind(this) as Dispatch<A>
+    return this.dispatch.bind(this) as Dispatch<A>;
   }
 }
 
-const ADD = 'ADD'
-const CHAT = 'CHAT'
+const ADD = 'ADD';
+const CHAT = 'CHAT';
 
-type AddType = typeof ADD
-type ChatType = typeof CHAT
+type AddType = typeof ADD;
+type ChatType = typeof CHAT;
 
 type ActionTypes =
   | {
-      type: AddType
-      payload: number
+      type: AddType;
+      payload: number;
     }
   | {
-      type: ChatType
-      payload: string
-    }
+      type: ChatType;
+      payload: string;
+    };
 
-type PickStorePayload<T> = PickPayload<ActionTypes, T>
+type PickStorePayload<T> = PickPayload<ActionTypes, T>;
 
 const store = new Vuex({
   state: {
@@ -121,10 +114,10 @@ const store = new Vuex({
   },
   action: {
     async [ADD](state, payload: PickStorePayload<AddType>) {
-      state.count += payload
+      state.count += payload;
     },
     async [CHAT](state, message: PickStorePayload<ChatType>) {
-      state.message = message
+      state.message = message;
     },
   },
   getters: {
@@ -135,7 +128,7 @@ const store = new Vuex({
       return `Hey! ${state.message}`
     },
   },
-})
+});
 
 store.subscribeAction({
   before: (action, state, getters) => {
@@ -143,26 +136,26 @@ store.subscribeAction({
       `before action ${action.type}, before state is ${JSON.stringify(
         state,
       )}, before getter is ${JSON.stringify(getters)}`,
-    )
+    );
   },
   after: (action, state, getters) => {
     console.log(
       `after action ${action.type},  after state is ${JSON.stringify(
         state,
       )}, after getter is ${JSON.stringify(getters)}`,
-    )
+    );
   },
-})
+});
 
 // for TypeScript support
-const dispatch = store.createDispatch<ActionTypes>()
+const dispatch = store.createDispatch<ActionTypes>();
 
 dispatch({
   type: ADD,
   payload: 3,
-})
+});
 
 dispatch({
   type: CHAT,
   payload: 'Hello World',
-})
+});
